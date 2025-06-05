aaa06876a16910caae03347939eaf1f590eb5d35
Bruno Ramos <robrumi01@gmail.com>
2025-06-05 00:48:48 -0300

    Variáveis

---
+++ Aula01/Aula04/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula04;
+
+public class Program
+{
+    public static void Main()
+    {
+        string myName = "Bruno";
+        int Idade = 22;
+        Double altura = 1.71;
+        bool isTrue = true;
+        char letra = 'B';
+       
+        Console.WriteLine($"Meu nome é {myName}, tenho {Idade} anos, minha altura é {altura}m, é {isTrue} que eu gosto de programar e a primeira letra do meu nome é {letra}.");
+    }
+}
