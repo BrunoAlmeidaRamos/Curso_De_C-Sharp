231eb87ff703ba6489d113836b4303a4efd02025
Bruno Ramos <robrumi01@gmail.com>
2025-06-05 22:52:56 -0300

    correção

--- Aula01/Aula04/Program.cs
+++ Aula01/Aula04/Program.cs
@@ -6,10 +6,10 @@ public class Program
     {
         string myName = "Bruno";
         int Idade = 22;
-        Double altura = 1.71;
+        double altura = 1.71; // é um tipo de dado numérico usado para representar números com casas decimais, ou seja, números reais.
         bool isTrue = true;
-        char letra = 'B';
-       
+        char letra = 'B'; //é um tipo de dado que armazena um único caractere. Ou seja, ele guarda letras, símbolos ou números individuais — um só de cada vez.
+
         Console.WriteLine($"Meu nome é {myName}, tenho {Idade} anos, minha altura é {altura}m, é {isTrue} que eu gosto de programar e a primeira letra do meu nome é {letra}.");
     }
 }
