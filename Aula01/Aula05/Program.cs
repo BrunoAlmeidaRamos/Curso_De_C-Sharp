6d6190329b58b7ec8266d1f0941a2371a4f693db
Bruno Ramos <robrumi01@gmail.com>
2025-06-06 23:29:31 -0300

    Input e Output

---
+++ Aula01/Aula05/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula05;
+
+public class  program
+{
+    public static void Main()
+    {
+        Console.WriteLine("-------CADASTRO-DE-USUARIO--------");
+        Console.WriteLine("Digite Seu Nome: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Digite Sua Idade: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("De Qual Cidade Você É? ");
+        string city = Console.ReadLine();
+
+        Console.WriteLine("-------CADASTRO-CONCLUIDO--------");
+
+        Console.WriteLine($"Seja bem vindo {name}!");
+        Console.WriteLine($"Sua idade foi registrado com sucesso: [{age} Anos]");
+        Console.WriteLine($"Sua cidade foi registrada com sucesso: [{city}]");
+    }
+}
