bcd3a18b7cf3047a9359a70b40b0a995162ac95a
Bruno Ramos <robrumi01@gmail.com>
2025-06-10 00:39:38 -0300

    Operadores Relacionais

---
+++ Aula01/Aula08/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Aula08; 
+
+    public class Program()
+    {
+        public static void Main()
+
+        {
+        Console.WriteLine("============ Operadores Relacionais===============\n");
+        Console.WriteLine("Digite o primeiro número:");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Difite o segundo número:");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        if (x == y)
+        {
+            Console.WriteLine($"Os números {x} e {y} são iguais!");
+        }
+        else
+        {
+            Console.WriteLine($"Ôs números {x} e {y} são diferentes");
+        }
+    }
+    
+      
+    }
+   
+       
+
+  
+
