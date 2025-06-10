2708f92920ec1bfadf490a726eca77f59f2c4a29
Bruno Ramos <robrumi01@gmail.com>
2025-06-09 23:32:00 -0300

    Estrutura Condicional: If-Else

--- Aula01/Aula07/Program.cs
+++ Aula01/Aula07/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Aula07
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("=========Verificar números pares e ímpares=========");
+            Console.Write("Digite um número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = numero % 2;
+
+                if (resultado == 0)
+            {
+                Console.WriteLine($"O número {numero} é par!");
+            } else
+            {
+                Console.WriteLine($"O número {numero} é impar!");
+            }
+
+        }
+    }
+
+
+}
