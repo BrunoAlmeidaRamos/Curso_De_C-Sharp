5d8e7f3d3ca2a2919f017523403aa183a1ac9b5d
Bruno Ramos <robrumi01@gmail.com>
2025-06-11 01:16:10 -0300

    For Loop

---
+++ Aula01/Aula13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Aula13;
+
+public class Program
+{
+    public static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+                Console.WriteLine($" {i} X {j} = {i * j}");
+            Console.WriteLine("================");
+        }
+    }
+}
