7322f509527fc7e45e1ecf30b4d2ac3021dd2d40
Bruno Ramos <robrumi01@gmail.com>
2025-06-11 01:25:55 -0300

    Foreach

---
+++ Aula01/Aula14/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Aula14;
+
+public class  Program 
+{
+    public static void Main()
+    {
+        List<int> itens = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        foreach (var numeros in itens)
+            Console.WriteLine(numeros);
+    }
+}
