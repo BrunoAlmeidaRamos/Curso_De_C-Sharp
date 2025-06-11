5d811e87ddacc49d85b3eac80f27dc7aadbe0506
Bruno Ramos <robrumi01@gmail.com>
2025-06-11 02:10:11 -0300

    List (listas)

--- Aula01/Aula14/Program.cs
+++ Aula01/Aula14/Program.cs
@@ -4,7 +4,7 @@ public class  Program
 {
     public static void Main()
     {
-        List<int> itens = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        List<int> itens = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         foreach (var numeros in itens)
             Console.WriteLine(numeros);
     }
---
+++ Aula01/Aula15/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Aula15;
+
+public class  Program
+{
+    public static void Main()
+    {
+        string linha = "====================";
+
+        List<string> platinas = new List<string>
+        {
+            "God of War",
+            "God of War 2",
+            "God of War 3",
+            "God of War 4"
+        };
+
+        Console.WriteLine(platinas[1]);
+
+        Console.WriteLine(linha);
+
+        Console.WriteLine("Platinas do God of War:\n");
+        foreach (var war in platinas)
+        {
+            Console.WriteLine(war);
+        }
+
+        Console.WriteLine(linha);
+
+        platinas.Add("God of War Ragnarok");
+        Console.WriteLine("Adicionando God of War Ragnarok:\n");
+        foreach (var war in platinas)
+        {
+            Console.WriteLine(war);
+        }
+
+        Console.WriteLine(linha);
+
+        Console.WriteLine($"Total de Platinas da saga God of War: {platinas.Count}\n");
+    }
+}
