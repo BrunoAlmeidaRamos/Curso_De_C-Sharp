a9eb267f355b50af74d3c6d2c10ca3799cb7abe7
Bruno Ramos <robrumi01@gmail.com>
2025-06-17 22:27:11 -0300

    Construtores, Programação Orientada a Objetos

---
+++ Aula01/Aula19/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Aula19;
+
+public class Program
+{
+    public static void Main()
+    {
+        Cake cakeChocolate = new Cake("Chocolate", "Prestigio", "Chocolate", 10, 150);
+
+        Console.WriteLine($"Sabor : {cakeChocolate.Sabor}");
+        Console.WriteLine($"Recheio : {cakeChocolate.Recheio}");
+        Console.WriteLine($"Cobertura : {cakeChocolate.Cobertura}");
+        Console.WriteLine($"Tamanho : {cakeChocolate.Tamanho} KG");
+        Console.WriteLine($"Valor : R$ {cakeChocolate.Valor}");
+
+        Console.WriteLine("==========================================");
+
+        Cake cakeNinho = new Cake("Ninho", "Nutela", "Ninho", 20, 250);
+
+        Console.WriteLine($"Sabor : {cakeNinho.Sabor}");
+        Console.WriteLine($"Recheio : {cakeNinho.Recheio}");
+        Console.WriteLine($"Cobertura : {cakeNinho.Cobertura}");
+        Console.WriteLine($"Tamanho : {cakeNinho.Tamanho} KG");
+        Console.WriteLine($"Valor : R$ {cakeNinho.Valor}");
+
+    }
+}
---
+++ Aula01/Aula19/cake.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Aula19
+{
+    public class Cake
+    {
+        public string Sabor;
+        public string Recheio;
+        public string Cobertura;
+        public int Tamanho;
+        public decimal Valor;
+
+        public Cake(string sabor, string recheio, string cobertura, int tamanho, decimal valor)
+
+        {
+            Sabor = sabor;
+            Recheio = recheio;
+            Cobertura = cobertura;
+            Tamanho = tamanho;
+            Valor = valor;
+
+        }
+    }
+}
